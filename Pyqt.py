ef13d43c4418a7d117218209a9eeaf1a52ffc2df
CheesyBoi66 <jtw7147@rit.edu>
2025-01-28 11:15:47 -0500

    woof

--- Pyqt.py
+++ Pyqt.py
@@ -1,2 +1,23 @@
-import time, math, time, os
-import PyQt5 as py
+import time, math, time, os, cv2 as w
+import Pyqt as py
+import numpy as np
+
+fps = 60
+
+
+def winInit():
+    global fRate
+    fRate = (1000//fps)
+    w.namedWindow("Bongo",w.WINDOW_NORMAL)
+    bak = np.ones((400,400,3), dtype=np.uint8) * 255
+    w.imshow("Bongo", bak)
+    
+def main():
+    
+    loop = True
+    winInit()
+
+    while(loop==True):
+        w.waitKey(fRate)
+
+main()
