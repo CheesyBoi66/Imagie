4d8a941638ef89482fb1cb9f669bf9e7e4f75c1d
CheesyBoi66 <jtw7147@rit.edu>
2025-01-30 13:18:44 -0500

    jijijjiuj

--- Pyqt.py
+++ Pyqt.py
@@ -2,21 +2,38 @@ import cv2
 import numpy as np
 from keyboard import is_pressed
 
+x,y = 0,0
+w,h = 1920,200
+
 def init():
-    cv2.namedWindow("Goofy moe", cv2.WINDOW_NORMAL)
     winImg = np.ones((1080, 1920, 3), dtype=np.uint8) * 255
-    shquare = cv2.rectangle(winImg,(50,100),(200,150),(0,100,255), -1)
-    cv2.imshow("Goofy moe", shquare)
+    cv2.rectangle(winImg,(x,y),(x+w,y+h),(0,100,255), -1)
     cv2.imshow("Goofy moe", winImg)
     
 
+
+
 def main():  
+    global x,y
+    
+    cv2.namedWindow("Goofy moe", cv2.WINDOW_NORMAL)
+    
+
     init()
     
     while True:
-        cv2.waitKey(1) & 0xFF  # Process OpenCV events
-        
-        
+        key = cv2.waitKey(1) & 0xFF  # Process OpenCV events
+
+        if is_pressed('w'):
+            y -= 2
+            init()
+            print("w")    
+
+
+        if is_pressed('s'):
+            y += 2
+            init()
+            print("s")
 
         if is_pressed('q'):  # Exit if 'q' is pressed
             break  
