91db53789e744479a82240aef8ea211cfa51d7bd
CheesyBoi66 <jtw7147@rit.edu>
2025-01-29 23:45:44 -0500

    tweaked and removesd soos

--- Pyqt.py
+++ Pyqt.py
@@ -5,21 +5,25 @@ from keyboard import is_pressed
 def init():
     cv2.namedWindow("Goofy moe", cv2.WINDOW_NORMAL)
     winImg = np.ones((1080, 1920, 3), dtype=np.uint8) * 255
+    shquare = cv2.rectangle(winImg,(50,100),(200,150),(0,100,255), -1)
+    cv2.imshow("Goofy moe", shquare)
     cv2.imshow("Goofy moe", winImg)
     
 
-def main():
+def main():  
     init()
+    
     while True:
         cv2.waitKey(1) & 0xFF  # Process OpenCV events
-
-        if is_pressed('w'):
-            print("soos")
+        
+        
 
         if is_pressed('q'):  # Exit if 'q' is pressed
             break  
 
     cv2.destroyAllWindows()
+
+
 
 if __name__ == "__main__":
     main()
